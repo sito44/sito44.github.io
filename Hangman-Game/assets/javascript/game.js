fe45bf868cb1feacb5a87f49f046be6904f89684
MacbookPro <andreesito@gmail.com>
2017-11-14 17:27:03 -0800

    css game theme change, js code added

--- Hangman-Game/assets/javascript/game.js
+++ Hangman-Game/assets/javascript/game.js
@@ -13,34 +13,66 @@ var techWords = [
 	'recursion', 
 	'closures'];
 
+var turn = 6;
 var currentWord = randomWordGenerator();
-var underScores;
+var totalUnderscore = [];
+var printedUnderScores;
+var letter;
+var CharacterAt;
+var wordLength = currentWord.length;
 console.log(currentWord);
 console.log(underScoreGenerator(currentWord));
+console.log(CharacterAt);
+
+
+document.onkeyup = function(event) {
+        letter = event.key;
+        console.log(letter);
+        letterCheckerAndPrint(letter);
+      };
 
 function $(id) {
 	return document.getElementById(id);
-};
-
+}
 
 
 function randomWordGenerator() {
 	var RN = techWords[Math.floor(Math.random() * techWords.length)];
 	return RN;
 }
+
 function underScoreGenerator(currentWord) {
 		var underScore = '_ ';
-		var totalUnderscore = [];
 		var multiple = currentWord.length;
 		for (var i = 0; i < multiple; i++) {
 			totalUnderscore.push(underScore);
 		}
-		$('hangWord').textContent = totalUnderscore;
+		 	printedUnderScores = totalUnderscore.join(' ');
+			console.log(totalUnderscore);
+			$('hangWord').textContent = printedUnderScores;
 		
 
-console.log(totalUnderscore);
+
 }
-/*document.onkeyup = function(event) {
-        var l = event.key;
-        console.log(l);
-      };*/
+
+function letterCheckerAndPrint(letter) {
+	for ( var index in currentWord) {
+		if (index = letter) {
+			CharacterAt = currentWord.indexOf(letter);
+			console.log(CharacterAt);
+			totalUnderscore.splice(CharacterAt, CharacterAt + 1,letter);
+			console.log(totalUnderscore);
+			break;
+			/*printedUnderScores = printedUnderScores.splice(CharacterAt,CharacterAt + 1, letter);
+			$('hangWord').textContent = printedUnderScores;*/
+			
+		} else if (index != letter){
+			turn = turn - 1;
+			$('turns').textContent = turn;
+			break;
+		}
+
+	}
+}
+console.log();
+			
