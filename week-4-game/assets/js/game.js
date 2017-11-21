ff1744f1009442f8c561cb003ba6bae297bdf884
MacbookPro <andreesito@gmail.com>
2017-11-20 18:37:44 -0800

    UI created

---
+++ week-4-game/assets/js/game.js
@@ -0,0 +1,15 @@
+'use strict';
+$(document).ready(function(){
+
+var fighter1 = $('#chr1');
+var fighter1Points = $('#chr1Points');
+var fighter2 = $('#chr2');
+var fighter2Points = $('#chr2Points');
+var fighter3 = $('#chr3');
+var fighter3Points = $('#chr3Points');
+var fighter4 = $('#chr4');
+var fighter4Points = $('#chr4Points');
+
+
+
+});
