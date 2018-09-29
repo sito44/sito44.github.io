d528cb6280052c43db952324abed3f86073b5ca0
Andre Holguin <andreesito@gmail.com>
2018-09-29 02:47:20 -0700

    Modal toggle on/off functionality created - Modal Content Functionality created - Refactored code from vanilla JS to JQuery

--- Hangman-Game/assets/javascript/game.js
+++ Hangman-Game/assets/javascript/game.js
@@ -1,194 +1,282 @@
 'use strict';
-let techWords = [
-	'switch',
-	'functions',
-	'objects',
-	'operators',
-	'booleans',
-	'hoisting',
-	'prototypes',
-	'parameters',
-	'methods',
-	'recursion',
-	'closures'
-];
+$(function () {
+	const $body = $('body');
 
-let win = 0;
-let turn;
-let currentWord;
-let totalUnderscore;
-let printed;
-let letter;
-let indices;
-let letterTrackerArray;
+	appendModalHtml();
+	// ------------------ Word Bank
+	let techWords = [
+		'switch',
+		'functions',
+		'objects',
+		'operators',
+		'booleans',
+		'hoisting',
+		'prototypes',
+		'parameters',
+		'methods',
+		'recursion',
+		'closures'
+	];
+	// ------------------ Application State
+	let win = 0;
+	let turn;
+	let currentWord;
+	let totalUnderscore;
+	let printed;
+	let letter;
+	let indices;
+	let letterTrackerArray;
+	// ----------------------
 
-function initialize() {
-	
-	turn = 6;
-	currentWord = randomWordGenerator();
-	totalUnderscore = [];
-	printed;
-	letter;
-	indices;
-	letterTrackerArray = [];
+	// ---------------------- Initialization
 
-	imageChange();
-	underScoreGenerator(currentWord);
-	$('wins').textContent = win;
-	$('hangWord').textContent = totalUnderscore.join(' ');
-	$('hangLetters').textContent = "";
-	$('turns').textContent = turn;
-	
-}
+	function appendModalHtml() {
 
-initialize();
-
-
-document.onkeyup = function (event) {
-	letter = event.key;
-	console.log(letter);
-	console.log(findIndices(currentWord));
-	console.log(letterTrackerArray.length);
-	letterTracker(letter);
-	letterCheckerAndPrint(letter);
-	imageChange();
-
-};
-
-function $(id) {
-	return document.getElementById(id);
-}
-
-
-function randomWordGenerator() {
-	let RN = techWords[Math.floor(Math.random() * techWords.length)];
-	return RN;
-}
-
-function underScoreGenerator(currentWord) {
-	let underScore = '_ ';
-	let multiple = currentWord.length;
-	for (let i = 0; i < multiple; i++) {
-		totalUnderscore.push(underScore);
+		$body.append(`
+	  <div class="modalOverlay">
+		<button class="closeButton">X</button>
+		<div class="modalContent">
+		</div>
+	  </div>
+    `);
 	}
 
-	printToScreen();
-}
+	function initialize() {
 
-function findIndices(indicesInWord) {
-	indices = [];
-	for (let i = 0; i < indicesInWord.length; i++) {
-		if (indicesInWord[i] === letter) indices.push(i);
-	}
-	return indices;
-}
+		turn = 6;
+		currentWord = randomWordGenerator();
+		totalUnderscore = [];
+		printed;
+		letter;
+		indices;
+		letterTrackerArray = [];
 
+		imageChange();
+		underScoreGenerator(currentWord);
+		$('#wins').text(win);
+		$('#hangWord').text(totalUnderscore.join(' '));
+		$('#hangLetters').text("");
+		$('#turns').text(turn);
 
-function printToScreen(z) {
-	printed = totalUnderscore.join(' ');
-	let wordEval = totalUnderscore.join('');
-	console.log(printed);
-	$('hangWord').textContent = printed;
-
-
-	if (wordEval === currentWord) {
-		win += 1;
-		$('wins').textContent = win;
-		initialize();
-	} else if (z === true) {
-		printed = totalUnderscore.join(' ');
-		$('hangWord').textContent = printed;
-
-	} else if (z === false) {
-		turn -= 1;
-		$('turns').textContent = turn;
-		if(turn === 0 ){
-			 initialize();
-
-			};
 	}
 
-	if (letterTrackerArray[0] === undefined) {
-		return;
-	} else {
-		let usedLetters = letterTrackerArray.join(' ');
-		console.log(usedLetters);
-		$('hangLetters').textContent = usedLetters;
+	initialize();
+
+	// --------------------------
+
+	// -------------------------- Key Binding
+
+	document.onkeyup = function (event) {
+		if (turn <= 0) return;
+		letter = event.key;
+		console.log(letter);
+		console.log(findIndices(currentWord));
+		console.log(letterTrackerArray.length);
+		letterTracker(letter);
+		letterCheckerAndPrint(letter);
+		imageChange();
+
+	};
+
+	// ---------------------------
+
+	// --------------------------- Word Generators
+
+	function randomWordGenerator() {
+		let RN = techWords[Math.floor(Math.random() * techWords.length)];
+		return RN;
 	}
-}
 
-function letterCheckerAndPrint(x) {
+	function underScoreGenerator(currentWord) {
+		let underScore = '_ ';
+		let multiple = currentWord.length;
+		for (let i = 0; i < multiple; i++) {
+			totalUnderscore.push(underScore);
+		}
 
-	let found = false;
+		printToScreen();
+	}
 
+	// ---------------------------
 
-	for (let w = 0; w < currentWord.length; w++) {
+	// --------------------------- Utils
 
-		if (currentWord[w] === x && found === false) {
+	function findIndices(indicesInWord) {
+		indices = [];
+		for (let i = 0; i < indicesInWord.length; i++) {
+			if (indicesInWord[i] === letter) indices.push(i);
+		}
+		return indices;
+	}
 
-			for (let r in indices) {
-				delete totalUnderscore[indices[r]];
-				totalUnderscore[indices[r]] = x;
-				console.log(r);
+	function letterCheckerAndPrint(x) {
+
+		let found = false;
+
+		for (let w = 0; w < currentWord.length; w++) {
+
+			if (currentWord[w] === x && found === false) {
+
+				for (let r in indices) {
+					delete totalUnderscore[indices[r]];
+					totalUnderscore[indices[r]] = x;
+					console.log(r);
+				}
+				found = true;
+				console.log(totalUnderscore);
 			}
-			found = true;
+		}
+		printToScreen(found);
+	}
 
-			console.log(totalUnderscore);
+	function printToScreen(z) {
+		printed = totalUnderscore.join(' ');
+		let wordEval = totalUnderscore.join('');
+		console.log(printed);
+		$('#hangWord').text(printed);
+
+		if (wordEval === currentWord) {
+			win += 1;
+			$('#wins').text(win);
+			modal(messageBank(currentWord));
+			setTimeout(() => {
+				initialize();
+			}, 1000);
+		} else if (z === true) {
+			printed = totalUnderscore.join(' ');
+			$('#hangWord').text(printed);
+
+		} else if (z === false) {
+			turn -= 1;
+			$('#turns').text(turn);
+			if (turn === 0) {
+				win = 0;
+				modal(messageBank(null));
+				setTimeout(() => {
+					initialize();
+				}, 1000);
+			};
+		}
+
+		if (letterTrackerArray[0] === undefined) {
+			return;
+		} else {
+			let usedLetters = letterTrackerArray.join(' ');
+			console.log(usedLetters);
+			$('#hangLetters').text(usedLetters);
 		}
 	}
-	printToScreen(found);
-}
 
+	function letterTracker(letter) {
 
-console.log(currentWord);
+		if (letterTrackerArray.length === 0) letterTrackerArray.push(letter);
 
+		let letterInArray = letterTrackerArray.find(l => {
+			if (l === letter) return true;
+		});
 
-function letterTracker(letter) {
-	if (letterTrackerArray.length === 0) {
-		letterTrackerArray.push(letter);
-	}
-	let letterInArray = letterTrackerArray.find(l => {
-		if (l === letter) {
-			return true;
+		if (letterInArray) {
+			return;
+		} else {
+			letterTrackerArray.push(letter);
 		}
-	});
-	if (letterInArray) {
-		return;
-	} else {
-		letterTrackerArray.push(letter);
 	}
 
-}
+	function imageChange() {
+		switch (turn) {
+			case 6:
+				$('#hangman').html('<img src="./assets/images/Hangman_game0.png">');
+				break;
+			case 5:
+				$('#hangman').html('<img src="./assets/images/Hangman_game1.png">');
+				break;
+			case 4:
+				$('#hangman').html('<img src="./assets/images/Hangman_game2.png">');
+				break;
+			case 3:
+				$('#hangman').html('<img src="./assets/images/Hangman_game3.png">');
+				break;
+			case 2:
+				$('#hangman').html('<img src="./assets/images/Hangman_game4.png">');
+				break;
+			case 1:
+				$('#hangman').html('<img src="./assets/images/Hangman_game5.png">');
+				break;
+			case 0:
+				$('#hangman').html('<img src="./assets/images/Hangman_game6.png">');
+				break;
 
-function imageChange() {
-	switch (turn) {
-		case 6:
-			$('hangman').innerHTML = '<img src="./assets/images/Hangman_game0.png">';
-			break;
-		case 5:
-			$('hangman').innerHTML = '<img src="./assets/images/Hangman_game1.png">';
-			break;
-		case 4:
-			$('hangman').innerHTML = '<img src="./assets/images/Hangman_game2.png">';
-			break;
-		case 3:
-			$('hangman').innerHTML = '<img src="./assets/images/Hangman_game3.png">';
-			break;
-		case 2:
-			$('hangman').innerHTML = '<img src="./assets/images/Hangman_game4.png">';
-			break;
-		case 1:
-			$('hangman').innerHTML = '<img src="./assets/images/Hangman_game5.png">';
-			break;
-		case 0:
-			$('hangman').innerHTML = '<img src="./assets/images/Hangman_game6.png">';
-			break;
 
+		}
+	}
+
+	// ------------------------------------ Modal
+
+	function modal(content) {
+		$('.modalContent').html(content);
+		$('.modalOverlay').addClass('showModal');
 
 	}
-}
 
-function modal() {
-	
-	
+});
+
+function messageBank(string) {
+	let result;
+	switch (string) {
+		case null:
+			result = `<h2>You Lose!</h2>`;
+			break;
+		case 'switch':
+			result = `<h2>${string}</h2>
+		<p>test</p>`;
+			break;
+		case 'functions':
+			result = `<h2>${string}</h2>
+		<p>test</p>`;
+			break;
+		case 'objects':
+			result = `<h2>${string}</h2>
+		<p>test</p>`;
+			break;
+		case 'operators':
+			result = `<h2>${string}</h2>
+		<p>test</p>`;
+			break;
+		case 'booleans':
+			result = `<h2>${string}</h2>
+		<p>test</p>`;
+			break;
+		case 'hoisting':
+			result = `<h2>${string}</h2>
+		<p>test</p>`;
+			break;
+		case 'prototypes':
+			result = `<h2>${string}</h2>
+		<p>test</p>`;
+			break;
+		case 'parameters':
+			result = `<h2>${string}</h2>
+		<p>test</p>`;
+			break;
+		case 'methods':
+			result = `<h2>${string}</h2>
+		<p>test</p>`;
+			break;
+		case 'recursion':
+			result = `<h2>${string}</h2>
+		<p>test</p>`;
+			break;
+		case 'closures':
+			result = `<h2>${string}</h2>
+		<p>test</p>`;
+			break;
+	}
+	return result;
 }
+// ------------------------------------
+
+// ------------------- Event Delegation
+
+$(document).on('click', '.closeButton', function () {
+	$('.modalOverlay').removeClass('showModal');
+});
